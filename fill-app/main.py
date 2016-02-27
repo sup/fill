3b23ad8642b9b89fe5a16608278e953ea8ec08de
Charles Lai <cjl223@cornell.edu>
2016-02-27 18:40:03 -0500

    add more css+MVC skeletons

--- fill-app/main.py
+++ fill-app/main.py
@@ -15,17 +15,46 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
-
+"""
+Main View Controllers
+"""
 @app.route('/')
 def home():
-    """Return a friendly HTTP greeting."""
+    """Return landing page if not logged in, else show dashboard"""
+    # TODO: Gatekeeper Controller
+    # if loggedin:
     return render_template('home.html', page_title="FILL")
+    # else: return render_template('dashboard.html', args*, kwargs**)
 
+# TODO: Login Controller
+@app.route('/login')
+def login():
+	pass
+
+# TODO: Signup
+@app.route('/signup')
+def signup():
+	pass
+
+# TODO: Create Event Controller
+
+# TODO: Join Event Controller
+
+# TODO: Show Events Controller
 @app.route('/events')
 def events():
 	"""Return event feed"""
 	return render_template('feed.html', page_title="Event Feed", events=[1,2,3])
 
+
+"""
+RESTful API for accessing NDB+Backend: search, etc -> useful with jQuery AJAX
+"""
+# TODO: Backend helper methods
+
+"""
+Error Handlers
+"""
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
