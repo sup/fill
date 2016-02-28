58173c99876ffd52f57b1c607e29287b6e9355c2
Charles Lai <cjl223@cornell.edu>
2016-02-28 09:31:12 -0500

    add username email and pass to user model

--- fill-app/models/user.py
+++ fill-app/models/user.py
@@ -6,7 +6,10 @@ User model for the FILL app.
 from google.appengine.ext import ndb
 
 class User(ndb.Model):
-	pass
+	# Properties
+    username = ndb.StringProperty(required = True)
+    password = ndb.StringProperty(required = True)
+    email = ndb.StringProperty(required = False)
 
 # Debug
 if __name__ == '__main__':
